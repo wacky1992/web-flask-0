8f84b064e112b79d2fcc2189dc956fea2a1221f6
wacky1992 <625327691@qq.com>
2019-05-08 15:20:37 +0800

    添加README文件，完善app.py文件

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask
+from flask import request
+# 引入命令行解析器
+from flask_script import Manager
+
+
 app = Flask(__name__)
+manager = Manager(app)
 
 
 # 修饰器定义路径，并确认返回值
 @app.route('/')
 def index():
     return 'hello world'
+
+
+# 修饰器可以通过引用上下文临时把某些对象变为全局可访问
+@app.route('/browser')
+def browser():
+    user_agent = request.headers.get('User-Agent')
+    return '<p>你的浏览器是：%s</p>' % user_agent
 
 
 # 修饰器定义路径并设置变量name，返回时根据变量name的值返回数值
@@ -15,4 +28,4 @@ def user(name):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run(debug=True)
