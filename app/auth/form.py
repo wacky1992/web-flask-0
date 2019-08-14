90bf4acef3e68df1372e0a02de2284ca1afea787
wacky1992 <625327691@qq.com>
2019-08-14 11:31:40 +0800

    用户注册

--- app/auth/form.py
+++ app/auth/form.py
@@ -1,10 +1,31 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, StringField, PasswordField, BooleanField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
+from wtforms import ValidationError
+from ..models import User
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Length(1, 32), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    email = StringField('邮箱', validators=[DataRequired(), Length(1, 32), Email()])
+    password = PasswordField('密码', validators=[DataRequired()])
     remember_me = BooleanField('记住')
     submit = SubmitField('登录')
+
+
+class SignupForm(FlaskForm):
+    email = StringField('邮箱', validators=[DataRequired(), Length(1, 32), Email()])
+    username = StringField('用户名', validators=[DataRequired(), Length(1, 32),
+                                              Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                                     'Usernames must have only letters, numbers, dots or '
+                                                     'underscores')])
+    password = PasswordField('密码', validators=[DataRequired(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+    submit = SubmitField('注册')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data.lower().first()):
+            raise ValidationError('Email already registered.')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use.')
--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@ from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from . import login_manager   # 加载用户的回调函数
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 
 
 class Role(db.Model):
@@ -21,6 +23,7 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(80))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     email = db.Column(db.String(30), unique=True, index=True)
+    confirmed = db.Column(db.Boolean, default=False)
 
     @property
     def password(self):
@@ -37,6 +40,26 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
     # check_password_hash(hash, password) •  ：这个函数的参数是从数据库中取回的密码散列
     # 值和用户输入的密码。返回值为 True 表明密码正确。
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id}).decode('utf-8')
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
+
+
+
+
 
     def __repr__(self):
         return '<User %r>' % self.username
